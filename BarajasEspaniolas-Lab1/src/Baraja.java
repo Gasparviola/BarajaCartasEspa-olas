39cc29fceca6a8dd1c3674f308408af3b524fad4
Gaspar Nicolas Viola <gaspar5454@hotmail.com>
2024-03-21 11:17:05 -0300

    Agregando metodos barajar(), siguienteCarta() y mostrarBaraja()

--- BarajasEspaniolas-Lab1/src/Baraja.java
+++ BarajasEspaniolas-Lab1/src/Baraja.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -15,15 +16,47 @@ public class Baraja {
           
             //Hacemos un for para que asigne un palo a cada carta
            for(String palo : palos){
-               //Usa
+               //Usamos otro for para asignar un numero (del 1 al 12, saltando al 8 y 9) a la carta
                for(int numero = 1; numero <= 12; numero++){
                    if(numero != 8 && numero != 9){
-                       cartas.add(new Carta(numero,palo));
+                       cartas.add(new Carta(numero,palo));//Aqui se envia el parametro de numero y palo a la carta.
                    }
                }
            }           
         }
         
+        //Con este metodo mezclamos la coleccion de cartas, "barajamos" justamente
+        public void barajar(){
+            //Este metodo se utiliza para barajar (o mezclar) los elementos de una colección. 
+            //En este caso, cartas es la colección que queremos barajar.
+            Collections.shuffle(cartas);
+        }
+        
+        
+        
+        //Con este metodo devolvemos la siguiente carta en la baraja
+        public Carta siguienteCarta(){
+            
+            //Verifica si la baraja de cartas (cartas) no está vacía utilizando el método isEmpty() de la colección cartas. 
+            //Si la baraja no está vacía, procede a lo siguiente.
+            if(!cartas.isEmpty()){
+                
+                //Utiliza el método remove(0) para eliminar y devolver la primera carta de la lista cartas. 
+                //Esto simula sacar la carta de la parte superior de la baraja.
+                return cartas.remove(0);
+            }else{
+                //Si la baraja está vacía, imprime un mensaje indicando que no hay más cartas disponibles y devuelve null.
+                System.out.println("No hay mas cartas en la baraja");
+                return null;
+            }
+        }
+        
+            //Con este metodo recorremos la lista y mostramos las cartas
+            public void mostrarBaraja() {
+            for (Carta carta : cartas) {
+                System.out.println(carta);
+                }
+            }
         
         
         
--- BarajasEspaniolas-Lab1/src/Main.java
+++ BarajasEspaniolas-Lab1/src/Main.java
@@ -4,6 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
         
+            //Creamos una baraja
+           Baraja baraja = new Baraja();
+            
+           System.out.println("Barajando el mazo de cartas...");
+           //Barajamos
+           baraja.barajar();
+           System.out.println("");
+           
+           System.out.println("La baraja ha quedado asi: ");
+           //Mostramos la baraja
+           baraja.mostrarBaraja();
+           System.out.println("");
+            
+        
     }
     
 }
